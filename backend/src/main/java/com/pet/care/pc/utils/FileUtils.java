fd98bf16c0782c10e95d1d28a00b873c499867c7
ahyeon1117 <ahyeon1117@naver.com>
2024-02-29 18:04:16 +0900

    [feat] webflux 커밋

--- backend/src/main/java/com/pet/care/pc/utils/FileUtils.java
+++ backend/src/main/java/com/pet/care/pc/utils/FileUtils.java
@@ -3,13 +3,16 @@ package com.pet.care.pc.utils;
 import com.pet.care.pc.enums.Connect;
 import com.pet.care.pc.enums.StatusEnum;
 import java.io.File;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.reactive.function.client.WebClient;
 
 public class FileUtils {
 
-  public StatusEnum upload(MultipartFile mFile, Connect connect) {
+  public StatusEnum upload(String path, MultipartFile mFile, Connect connect) {
     StatusEnum result = Connect.LOCAL.equals(connect)
-      ? localUpload(mFile)
+      ? localUpload(path, mFile)
       : apiUpload(mFile);
     return result;
   }
@@ -21,15 +24,41 @@ public class FileUtils {
     return result;
   }
 
+  // TODO : 수정 필요
   public StatusEnum apiUpload(MultipartFile mFile) {
-    return StatusEnum.COMPLETE;
+    StatusEnum result = StatusEnum.COMPLETE;
+    try {
+      WebClient
+        .builder()
+        .baseUrl("주소")
+        .build()
+        .get()
+        .retrieve()
+        .onStatus(
+          HttpStatusCode.valueOf(
+            HttpStatus.INTERNAL_SERVER_ERROR.value()
+          )::equals,
+          response -> response.bodyToMono(String.class).map(Exception::new)
+        );
+    } catch (Exception e) {
+      result = StatusEnum.FAILURE;
+    }
+    return result;
   }
 
-  public StatusEnum localUpload(MultipartFile mFile) {
+  // TODO : 경로 파라미터 규격 정의
+  public StatusEnum localUpload(String path, MultipartFile mFile) {
+    // new File();
     return StatusEnum.COMPLETE;
   }
 
   public File apiDownload(String path, String remoteUrl) {
+    // StatusEnum result = StatusEnum.COMPLETE;
+    // try {
+    // } catch (Exception e) {
+    //   result = StatusEnum.FAILURE;
+    // }
+    // return result;
     return new File(path);
   }
 
