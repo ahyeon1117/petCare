7eae3b2a908e86e1967b513c5527a8337a047af0
ahyeon1117 <ahyeon1117@naver.com>
2024-07-29 11:08:44 +0900

    [fix] userid_platform으로 id 조회 변경

--- backend/src/main/java/com/pet/care/pc/redis/service/TokenService.java
+++ backend/src/main/java/com/pet/care/pc/redis/service/TokenService.java
@@ -4,6 +4,8 @@ import com.pet.care.pc.dao.repository.redis.TokenRepository;
 import com.pet.care.pc.entitiy.user.id.UserId;
 import com.pet.care.pc.redis.entity.JwtToken;
 import com.pet.care.pc.redis.jwt.JwtTokenProvider;
+import com.pet.care.pc.utils.Utils;
+
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -24,14 +26,16 @@ public class TokenService {
     refreshTokenRepository.deleteByAccessToken(accessToken);
   }
 
-  public JwtToken generateJwtToken(UserId id, String role) {
-    String refreshToken = tokenProvider.generateRefreshToken(id, role);
-    String accessToken = tokenProvider.generateAccessToken(id, role);
+  public JwtToken generateJwtToken(String token, String role) {
+    String[] userIdArr = Utils.splitUnderscore(tokenProvider.getUid(token));
+    UserId userId = UserId.builder().userId(userIdArr[0]).platform(userIdArr[1]).build();
+    String refreshToken = tokenProvider.generateRefreshToken(userId, role);
+    String accessToken = tokenProvider.generateAccessToken(userId, role);
 
     JwtToken result = new JwtToken(
       accessToken,
-      id.getUserId(),
-      id.getPlatform(),
+      userId.getUserId(),
+      userId.getPlatform(),
       refreshToken
     );
 
